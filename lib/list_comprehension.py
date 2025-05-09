1c95de7fa1605402a61e5fdf53e1338e57dab07c
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-05-09 15:24:07 +0300

    add list comrehsion to pass test

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    """
+    Return a list of even numbers from the input list.
+
+    :param num_list: List of integers
+    :return: List of even integers
+    """
+    return [num for num in num_list if num % 2 == 0]
+    
+    
 
 def make_exclamation(sentence_list):
-    pass
+    
+    """
+    Append an exclamation mark to each sentence in the input list.
+
+    :param sentence_list: List of strings
+    :return: List of strings with exclamation marks
+    """
+    return [sentence + "!" for sentence in sentence_list]
